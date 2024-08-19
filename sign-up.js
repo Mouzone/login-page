c515e803d78e333408d9ed8a4998b14e84d93a1c
Mouzone <sunnyliu010@gmail.com>
2024-08-19 18:11:54 -0400

    Create errors for sign-up page form and properly check for confirmation emails and passwords

--- sign-up.js
+++ sign-up.js
@@ -7,15 +7,29 @@ const confirm_password_element = document.getElementById("sign-up-confirm-passwo
 const email_element = document.getElementById("sign-up-email")
 const confirm_email_element = document.getElementById("sign-up-confirm-email")
 
+const password_error = document.getElementById("password-error")
+const email_error = document.getElementById("email-error")
+
 signup_form.addEventListener("submit", event => {
     event.preventDefault()
-    socket.send(JSON.stringify({
-        action: "login",
-        email: email_element,
-        username: username_element.value,
-        password: password_element.value
-    }))
-    signup_form.reset()
+    if (confirm_password_element.value !== password_element.value) {
+        password_error.classList.add("active")
+        password_error.textContent = "Passwords are different"
+    } else if (confirm_email_element.value !== email_element.value) {
+        password_error.classList.remove("active")
+        password_error.textContent = ""
+        
+        email_error.classList.add("active")
+        email_error.textContent = "Emails are different"
+    } else {
+        socket.send(JSON.stringify({
+            action: "login",
+            email: email_element,
+            username: username_element.value,
+            password: password_element.value
+        }))
+        signup_form.reset()
+    }
 })
 
 socket.onmessage = async event => {
